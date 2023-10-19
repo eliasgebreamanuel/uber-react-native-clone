b3905bbdebc78b6f07b01984a4ea781821127e92
eliasgebreamanuel <eliasgamanuel@gmail.com>
2023-10-19 22:26:19 +0300

    map page commit

--- App.js
+++ App.js
@@ -3,12 +3,21 @@ import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator, StackView } from '@react-navigation/stack'; 
+import MapScreen from "./screens/MapScreen";
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <StackView.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
+            <Stack.Screen name ="MapScreen" component={MapScreen} options={{headerShown: false}} />
+          </StackView.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -29,8 +29,9 @@ const NavOptions = () => {
         style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
     >
         <View>
-            <Image style= {{ width: 100, height: 120, resizeMode: "contain" }} source= {{ uri: item.image }}/>
+            <Image style={{ width: 100, height: 120, resizeMode: "contain" }} source= {{ uri: item.image }}/>
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon style={tw`p-2 bg-black rounded-full w-18 mt-4`} name="arrowright" color="white" type="antdesign"/>
         </View>
     </TouchableOpacity>
    }}
